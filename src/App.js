4e93269a0062a27f321c084ac3045609b685a67a
JV Villanueva <jan@missionreadyhq.com>
2023-09-16 11:06:20 +1200

    basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [img, setImg] = useState("");
+
+  useEffect(() => {
+    // Define the URL of your backend API
+    const apiUrl = "http://localhost:4000/products"; // Replace with your actual API endpoint
+
+    // Make a GET request to retrieve products with Base64-encoded images
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        // Handle the successful response here
+        const fetchedProducts = response.data.products[0].image;
+        setImg(fetchedProducts);
+      })
+      .catch((error) => {
+        // Handle any errors here
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await axios.get("http://localhost:4000/products");
+  //     console.log(data);
+  //     setImg(data.data.products[0].image);
+  //     console.log(typeof data.data.products[0].image);
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src={`data:image/webp;base64,${img}`}
+        className="App-logo"
+        alt="logo"
+      />
     </div>
   );
 }
